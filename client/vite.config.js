9af38e74c23ec9213a8b219617121a106fc1c3f7
zeeshan ali <zeeshanali4704@gmail.com>
2026-07-24 21:08:26 +0500

    again change

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,10 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
-     tailwindcss(),
+    react(),
+    tailwindcss(),
   ],
-})
+});
